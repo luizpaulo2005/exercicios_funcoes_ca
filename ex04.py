dc1050dab51b758f480f0d62cb0dd83649bbb4a9
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-06-23 22:52:21 -0300

    ex04 atualizado

--- ex04.py
+++ ex04.py
@@ -1,17 +1,35 @@
 import math
 
+def calculaDiferenca(x, y):
+    if x > y:
+        return x - y
+    else:
+        return y - x
+
+def calculaQuadrado(x):
+    return x * x
 
 def calculaDistancia(x, y):
     return math.dist(x, y)
 
 
-caso1 = calculaDistancia([1, 2], [2, 3])
-print(caso1)
-caso2 = calculaDistancia([2, 6], [7, 2])
-print(caso2)
-caso3 = calculaDistancia([3, 4], [5, 6])
-print(caso3)
-caso4 = calculaDistancia([7, 2], [4, 6])
-print(caso4)
-caso5 = calculaDistancia([10, 9], [23, 1])
-print(caso5)
+caso0_1 = calculaDiferenca(2, 1)
+print(caso0_1)
+caso0_2 = calculaDiferenca(5, 3)
+print(caso0_2)
+caso0_3 = calculaDiferenca(10, 5)
+print(caso0_3)
+
+caso1_1 = calculaDistancia([1, 2], [2, 3])
+print(caso1_1)
+caso1_2 = calculaDistancia([2, 6], [7, 2])
+print(caso1_2)
+caso1_3 = calculaDistancia([3, 4], [5, 6])
+print(caso1_3)
+
+caso2_1 = calculaQuadrado(2)
+print(caso2_1)
+caso2_2 = calculaQuadrado(9)
+print(caso2_2)
+caso2_3 = calculaQuadrado(10)
+print(caso2_3)
